c7a6969d5c87d6cb850a017cf46d78e36fcf98a4
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-13 16:59:53 +0100

    alt attribute is now a empty string

--- src/components/WeatherImage.tsx
+++ src/components/WeatherImage.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function WeatherImage({ icon }: Props) {
   return (
     <Box>
-      <Image src={"https://openweathermap.org/img/wn/" + icon + ".png"} alt="Weather - Image" />
+      <Image src={"https://openweathermap.org/img/wn/" + icon + ".png"} alt="" />
     </Box>
   )
 }
