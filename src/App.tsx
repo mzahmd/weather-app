fdc7b820e87a75a6c6d9052f49ab62712e13fe63
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-13 12:02:49 +0100

    build the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { SearchIcon } from "@chakra-ui/icons"
+import { Box, Container, HStack, Input, InputGroup, InputLeftAddon, Stack, Text } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <>
-    </>
+    <Box p={5}>
+      <Box>
+        <InputGroup>
+          <InputLeftAddon>
+            <SearchIcon />
+          </InputLeftAddon>
+          <Input width='50%' placeholder="Enter a City..." />
+        </InputGroup>
+      </Box>
+      <Box mt={10} pb={10} border="2px" borderRadius={"10"} borderColor='gray.200'>
+        <Container>
+          <HStack spacing={"20rem"}>
+            <Box>Bild</Box>
+            <Box mt={10}>
+              <Stack>
+                <Text>Day</Text>
+                <Text as={"b"} fontSize={"4xl"} mb={2}>City</Text>
+                <Text>Temperatur</Text>
+                <Text>Info</Text>
+              </Stack>
+            </Box>
+          </HStack>
+        </Container>
+      </Box>
+      <Box>
+
+      </Box>
+    </Box>
   )
 }
 
