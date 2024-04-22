eeb3136e9794dd1537479f7cbf14c58d76542af8
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-04-22 14:06:43 +0200

    add alt attrbute

--- src/components/WeatherImage.tsx
+++ src/components/WeatherImage.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function WeatherImage({ icon }: Props) {
   return (
     <Box mb={0}>
-      <Image src={"https://openweathermap.org/img/wn/" + icon + ".png"} alt="" />
+      <Image src={"https://openweathermap.org/img/wn/" + icon + ".png"} alt="weather - image" />
     </Box>
   )
 }
