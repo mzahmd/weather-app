0bb53a3c4f37d13f929fd5ecc97cbba25ee43927
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-04-22 14:30:16 +0200

    refactor: use handleSubmit instead of handleKey

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
+
 import { SearchIcon } from "@chakra-ui/icons"
 import { Box, Container, HStack, Input, InputGroup, InputLeftAddon } from "@chakra-ui/react"
-import { useState } from "react"
+
 import ColorSwitch from "./components/ColorSwitch"
 import WeatherData from "./components/WeatherData"
 
@@ -8,22 +10,23 @@ function App() {
   const [isSearch, setIsSearch] = useState("")
   const [searchCity, setSearchCity] = useState("")
 
-  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.code === "Enter" && isSearch.trim() !== "") {
-      setSearchCity(isSearch.trim())
-      setIsSearch("")
-    }
+  function handleSubmit(e: React.FormEvent<HTMLInputElement>) {
+    e.preventDefault()
+    setSearchCity(isSearch)
+    setIsSearch("")
   }
 
   return (
     <Box p={5}>
-      <HStack>
-        <InputGroup>
-          <InputLeftAddon>
-            <SearchIcon />
-          </InputLeftAddon>
-          <Input variant={"filled"} onChange={(e) => setIsSearch(e.target.value)} value={isSearch} placeholder={"Enter a City..."} onKeyDown={(e) => handleKeyPress(e)} />
-        </InputGroup>
+      <HStack justify={"center"}>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <InputGroup>
+            <InputLeftAddon>
+              <SearchIcon />
+            </InputLeftAddon>
+            <Input variant={"filled"} onChange={(e) => setIsSearch(e.target.value)} value={isSearch} placeholder={"Enter a City..."} />
+          </InputGroup>
+        </form>
         <ColorSwitch />
       </HStack>
       <Container mt={10} p={10} border={"2px"} borderRadius={"10"} borderColor={"gray.400"} centerContent>
