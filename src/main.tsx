f9a1f7adea29074204e5ac703601e4ac44b4f519
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-04-22 14:07:06 +0200

    removed unused import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react'
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
